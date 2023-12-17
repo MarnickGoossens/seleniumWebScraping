9d03d86b5a727edae62942e37aa294bcd9651510
Marnick Goossens <r0930150@student.thomasmore.be>
2023-12-17 17:56:25 +0100

    final

--- Program.cs
+++ Program.cs
@@ -1,16 +1,13 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Text.Json;
-using System.IO;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System;
 using System.Text;
-using System.Diagnostics;
+
 class Program
 {
     static void Main()
     {
+        // Gebruikersinput voor keuze en zoekterm
         Console.WriteLine("Kies uit de volgende keuzes:");
         Console.WriteLine("1. De 5 meest recent geüploade YouTube video’s op basis van een zoekterm");
         Console.WriteLine("2. De 5 nieuwste jobs te vinden op www.ictjob.be met zoekterm");
@@ -20,12 +17,16 @@ class Program
         Console.Write("Geef je zoekterm: ");
         string zoekterm = Console.ReadLine();
 
+        // Case: YouTube-scraping
         if (keuze == 1)
         {
+            // Initialisatie van ChromeDriver, navigatie naar YouTube en accepteren van cookies
             IWebDriver driver = new ChromeDriver();
             String url = "https://www.youtube.com/";
             driver.Navigate().GoToUrl(url);
             Thread.Sleep(2000);
+
+            // Elementen selecteren en interactie om zoekresultaten te verkrijgen
             List<string[]> items = new List<string[]>();
             IWebElement acceptAll = driver.FindElement(By.CssSelector("div.body.style-scope.ytd-consent-bump-v2-lightbox > div.eom-buttons.style-scope.ytd-consent-bump-v2-lightbox > div:nth-child(1) > ytd-button-renderer:nth-child(1) > yt-button-shape > button"));
             acceptAll.Click();
@@ -37,6 +38,7 @@ class Program
             // Wacht enkele seconden om de zoekresultaten te laden.
             Thread.Sleep(2000);
 
+            // Filteren op recente uploads en verzamelen van informatie over video's
             IWebElement filter = driver.FindElement(By.CssSelector("#filter-button > ytd-button-renderer > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill"));
             filter.Click();
             Thread.Sleep(2000);
@@ -44,32 +46,25 @@ class Program
             releaseDate.Click();
             Thread.Sleep(2000);
 
+            // Verzamelen van video-informatie en schrijven naar CSV en JSON
             IReadOnlyCollection<IWebElement> productElements = driver.FindElements(By.CssSelector("#contents > ytd-video-renderer"));
             List<video> videos = new List<video> {};
             foreach (IWebElement productElement in productElements.Take(5))
             {
-                // Het blok tekst splitsen in een array van strings op basis van nieuwe regeltekens
-                string[] regels = productElement.Text.Split(new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-                // Een lijst maken en de regels toevoegen
-                List<string> regelLijst = new List<string>(regels);
-
-                // Inhoud van de lijst afdrukken
-                Console.WriteLine("Inhoud van de lijst:");
-                foreach (string regel in regelLijst)
-                {
-                    Console.WriteLine(regel);
-                }
+                // Informatie extraheren van video-elementen
                 string title = productElement.FindElement(By.Id("video-title")).Text;
                 string link = productElement.FindElement(By.Id("video-title")).GetAttribute("href");
-                string uploaded = productElement.FindElement(By.CssSelector("#metadata-line > span:nth-child(4)")).Text; 
-                
+                string uploaded = productElement.FindElement(By.CssSelector("#metadata-line > span:nth-child(4)")).Text;
+
+                // Opslaan van informatie in lijsten en objecten
                 items.Add(new string[] { title, uploaded, link });
                 videos.Add(new video { videoTitle = title, videoUploaded = uploaded, videoLink = link });
+
+                // Schrijven naar CSV-bestand
                 string csvFilePath = "C:\\Users\\marni\\source\\repos\\webscraping\\YouTube.csv";
                 using (StreamWriter writer = new StreamWriter(csvFilePath))
                 {
-                    writer.WriteLine("Title,uploader,uploaded,link");
+                    writer.WriteLine("Title,uploaded,link");
                     foreach (string[] item in items)
                     {   
                         writer.WriteLine(string.Join(",", item));
@@ -87,12 +82,17 @@ class Program
             // Sluit de browser.
             driver.Quit();
         }
+        
+        // Case: ICTJob.be-scraping
         else if (keuze == 2)
         {
+            // Initialisatie van ChromeDriver, navigatie naar ICTJob.be en zoekopdracht
             IWebDriver driver = new ChromeDriver();
             String url = "https://www.ictjob.be/nl/";
             driver.Navigate().GoToUrl(url);
             Thread.Sleep(2000);
+
+            // Elementen selecteren en interactie om zoekresultaten te verkrijgen
             List<string[]> items = new List<string[]>();
             IWebElement searchField = driver.FindElement(By.Id("keywords-input"));
             IWebElement searchButton = driver.FindElement(By.Id("main-search-button"));
@@ -101,17 +101,23 @@ class Program
             searchButton.Click();
             Thread.Sleep(20000);
 
+            // Verzamelen van vacature-informatie en schrijven naar CSV en JSON
             IReadOnlyCollection<IWebElement> productElements = driver.FindElements(By.ClassName("job-info"));
             List<vacature> vacatures = new List<vacature> { };
             foreach (IWebElement productElement in productElements.Take(5))
             {
+                // Informatie extraheren van vacature-elementen
                 string title = productElement.FindElement(By.ClassName("job-title")).Text;
                 string company = productElement.FindElement(By.ClassName("job-company")).Text;
                 string location = productElement.FindElement(By.ClassName("job-location")).Text;
                 string keywords = productElement.FindElement(By.CssSelector("span:nth-child(4)")).Text.Replace(",", ";");
                 string link = productElement.FindElement(By.ClassName("job-title")).GetAttribute("href");
+
+                // Opslaan van informatie in lijsten en objecten
                 vacatures.Add(new vacature { jobTitle = title, jobCompany = company, jobLocation = location, jobKeywords = keywords, jobLink = link, });
                 items.Add(new string[] { title, company, location, keywords, link });
+
+                // Schrijven naar CSV-bestand
                 string csvFilePath = "C:\\Users\\marni\\source\\repos\\webscraping\\vacatures.csv";
                 using (StreamWriter writer = new StreamWriter(csvFilePath))
                 {
@@ -131,10 +137,15 @@ class Program
 
             driver.Quit();
         }
+
+        // Case: Bol.com-scraping
         else if (keuze == 3) {
+            // Initialisatie van ChromeDriver, navigatie naar bol.com en zoekopdracht
             IWebDriver driver = new ChromeDriver();
             String url = "https://www.bol.com/nl/nl/";
             driver.Navigate().GoToUrl(url);
+
+            // Elementen selecteren en interactie om zoekresultaten te verkrijgen
             List<string[]> items = new List<string[]>();
             Thread.Sleep(2000);
             IWebElement acceptAll = driver.FindElement(By.Id("js-first-screen-accept-all-button"));
@@ -148,14 +159,20 @@ class Program
             searchField.SendKeys(Keys.Enter);
             Thread.Sleep(2000);
 
+            // Verzamelen van product-informatie en schrijven naar CSV en JSON
             IReadOnlyCollection<IWebElement> productElements = driver.FindElements(By.ClassName("product-item--row"));
             List<product> producten = new List<product> { };
             foreach (IWebElement productElement in productElements.Take(3))
             {
+                // Informatie extraheren van product-elementen
                 string title = productElement.FindElement(By.ClassName("product-title--inline")).Text;
                 string price = productElement.FindElement(By.ClassName("promo-price")).Text.Replace("\r", ".").Replace("\n", "");
+
+                // Opslaan van informatie in lijsten en objecten
                 producten.Add(new product { productTitle = title, productPrice = price });
                 items.Add(new string[] { title, price });
+
+                // Schrijven naar CSV-bestand
                 string csvFilePath = "C:\\Users\\marni\\source\\repos\\webscraping\\bol.csv";
                 using (StreamWriter writer = new StreamWriter(csvFilePath))
                 {
@@ -179,8 +196,6 @@ class Program
     }
 }
 
-// Define a simple class for demonstration
-// Define een eenvoudige klasse voor demonstratie
 
 public class video
 {
